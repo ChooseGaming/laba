c997e488d89d39593532acc9b5d8766a201031b0
_MorZe_ <tanos22818@gmail.com>
2024-12-30 18:40:38 +0600

    gg

--- 25.11.2024 (НЕ ГОТОВО)/main/Program.cs
+++ 25.11.2024 (НЕ ГОТОВО)/main/Program.cs
@@ -5,32 +5,69 @@ class prog
     static void Main()
     {
         string txt = RL().ToLower();
-        string prev = "0";
-        int strike = 1, MaxStrike = 0;
-        string MostPopular = "0";
+        string symbols = "";
+        string hmuch = "";
+        int strike = 0, MaxStrike = 0;
         for (int i = 0; i < txt.Length; i++)
         {
-            if (i==0)
+            if (txt[i] == 'a')
             {
-                prev = txt[i].ToString();
-                MostPopular = txt[i].ToString();
-            }
-            else if (prev == txt[i].ToString())
-            {
-                strike += 1;
-            } else
-            {
-                if (MaxStrike < strike)
+                strike++;
+                i++;
+                if (i < txt.Length && txt[i] == 'b')
                 {
-                    MaxStrike = strike;
-                    MostPopular = txt[i].ToString();
+                    strike++;
+                    i++;
+                    if (i < txt.Length && txt[i] == 'c')
+                    {
+                        strike++;
+                    }
+                    else
+                    {
+                        MaxStrike = MaxStrike < strike ? strike : MaxStrike;
+                        strike = 0;
+                    }
                 }
-                prev = txt[i].ToString();
-                strike = 1;
+                else
+                {
+                    MaxStrike = MaxStrike < strike ? strike : MaxStrike;
+                    strike = 0;
+                }
+            } 
+            else
+            {
+                MaxStrike = MaxStrike < strike ? strike : MaxStrike;
+                strike = 0;
             }
         }
-        Console.WriteLine($"Самый частый символ, учавствующий в комбинации a*b: {MostPopular} ({MaxStrike})");
-        
+        MaxStrike = MaxStrike < strike ? strike : MaxStrike;
+        if (txt.Length >= 3) for (int i = 2; i < txt.Length; i++)
+            {
+                if (txt[i-2] == 'a' && txt[i] == 'b')
+                {
+                    if (!symbols.Contains(txt[i - 1].ToString())) symbols += $"{txt[i - 1]} ";
+                    hmuch += txt[i - 1];
+                }
+            }
+        string[] listOfS = symbols.Split(' ');
+        int Max = 0;
+        char S = ' ';
+        for (int i = 0; i < listOfS.Length-1; i++)
+        {
+            if (i == 0)
+            {
+                Max = hmuch.Split(listOfS[i][0]).Length - 1;
+                S = listOfS[i][0];
+                continue;
+            }
+            if (Max < hmuch.Split(listOfS[i][0]).Length - 1)
+            {
+                Max = hmuch.Split(listOfS[i][0]).Length - 1;
+                S = listOfS[i][0];
+            }
+        }
+        Console.WriteLine($"Наиболее часто встречаемый символ: \"{S}\" ({Max})");
+        Console.WriteLine($"Наибольшее количество подряд идущих abc: {MaxStrike}");
     }
 
     static string RL()
@@ -42,3 +79,11 @@ class prog
         return txt;
     }
 }
+
+
+//На вход подается строка. Необходимо определить  символ, который чаще всего
+//встречается в комбинации a*b. Регистр не учитывается.
+
+//На вход подается строка. Определить максимальную длину подпоследовательности,
+//состоящей из троек элементов abc. 
+
