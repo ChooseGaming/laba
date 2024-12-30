00e4bb635b189f08616a2cd5dd6673d7de19766c
_MorZe_ <tanos22818@gmail.com>
2024-12-30 17:58:34 +0600

    mm

---
+++ 25.11.2024 (НЕ ГОТОВО)/main/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class prog
+{
+    static void Main()
+    {
+        string txt = RL().ToLower();
+        string prev = "0";
+        int strike = 1, MaxStrike = 0;
+        string MostPopular = "0";
+        for (int i = 0; i < txt.Length; i++)
+        {
+            if (i==0)
+            {
+                prev = txt[i].ToString();
+                MostPopular = txt[i].ToString();
+            }
+            else if (prev == txt[i].ToString())
+            {
+                strike += 1;
+            } else
+            {
+                if (MaxStrike < strike)
+                {
+                    MaxStrike = strike;
+                    MostPopular = txt[i].ToString();
+                }
+                prev = txt[i].ToString();
+                strike = 1;
+            }
+        }
+        Console.WriteLine($"Самый частый символ, учавствующий в комбинации a*b: {MostPopular} ({MaxStrike})");
+        
+    }
+
+    static string RL()
+    {
+        Console.WriteLine("Введите строку");
+        string txt = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine(txt);
+        return txt;
+    }
+}
